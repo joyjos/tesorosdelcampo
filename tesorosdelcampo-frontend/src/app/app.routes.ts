1fda7f6fca3b35f05b94b8cddc8df7fc5f6ec7c7
Bedknobs <noeliareysilva@gmail.com>
2024-03-14 11:22:45 +0100

    Add routes

--- tesorosdelcampo-frontend/src/app/app.routes.ts
+++ tesorosdelcampo-frontend/src/app/app.routes.ts
@@ -1,6 +1,8 @@
 import { Routes } from '@angular/router';
 import { AppComponent } from './app.component';
+import { ArticlesComponent } from './articles/articles.component';
 
 export const routes: Routes = [
-    { path: 'home', component: AppComponent }
+    { path: 'home', component: AppComponent },
+    { path: 'articles', component: ArticlesComponent }
 ];
